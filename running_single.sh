2dc0c2c3a57e962b3f66d8d5aaf89d2e4f8a1090
Rece Coffin <rcoffin@mymail.mines.edu>
2018-04-16 18:21:14 -0600

    Updated scripts

--- running_single.sh
+++ running_single.sh
@@ -1,9 +1,17 @@
+unzip *.zip
+cd *
+cd A5
+g++ -std=c++0x *.cpp
+./a.out
+cd ..
+
 for d in $(ls -d */); do
     cd $d
     pwd
-    g++ -std=c++0x *.cpp
-    ./a.out
     gedit *.cpp
     cd ..
 done
+cd ~/Downloads
+rm -r Set5*
 exit 0
+
