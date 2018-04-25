982872d55bf667f22eb56468d01ff4f56c25d43c
Rece Coffin <rcoffin@mymail.mines.edu>
2018-04-25 11:28:30 -0600

    Added file to grade tests

---
+++ gradeTests.sh
@@ -0,0 +1,13 @@
+# Program to loop through the test
+# submissions and open the code to read it
+
+# Author : Rece Coffin
+for i in $(ls -d */); do
+  cd $i
+  #Enter sub folder
+  cd exam2probf
+  gedit solution.cpp
+  cd ..
+  cd ..
+done
+exit 0
